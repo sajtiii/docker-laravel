165088856d4c636d3ac66cc38a98d110c5186ece
Attila Tamás Nagy <49076408+sajtiii@users.noreply.github.com>
2024-05-06 14:11:32 +0200

    Update healthcheck.sh to utilize the health endpoint

--- overlay/healthcheck.sh
+++ overlay/healthcheck.sh
@@ -1,5 +1,5 @@
 if [[ $CONTAINER_ROLE == *"web"*  ]]; then
-    php artisan octane:status || exit 1
+    (php artisan octane:status || exit 1) && (curl --fail "${HEALTHCHECK_URL:-localhost/up}" || exit 1)
 fi
 
 exit 0;
