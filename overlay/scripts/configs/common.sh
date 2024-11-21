f06c4a6868bee270cec8906f098310e0030b8adf
Attila Tamas Nagy <nagyattila949@gmail.com>
2024-11-21 11:04:46 +0100

    Explode common config

--- overlay/scripts/configs/common.sh
+++ overlay/scripts/configs/common.sh
@@ -1,8 +1,6 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # Set default env vars
 export CONTAINER_ROLE=${CONTAINER_ROLE:-web,queue,scheduler}
 
 export APP_PATH=${APP_PATH:-/srv/http}
-
-export APP_ENV=${APP_ENV:-production}
