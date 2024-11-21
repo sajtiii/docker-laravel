e420a163e99e61bfe5a07fbbe5ef1c01879d5443
Attila Tamas Nagy <nagyattila949@gmail.com>
2024-11-21 11:05:16 +0100

    Add -e and empty line to configs

--- overlay/scripts/configs/queue.sh
+++ overlay/scripts/configs/queue.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 export QUEUES=${QUEUES:-high,medium,notification,default,low}
 export QUEUE_TRIES=${QUEUE_TRIES:-3}
--- overlay/scripts/configs/web.sh
+++ overlay/scripts/configs/web.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 export PORT=${PORT:-80}
 export CADDY_ADMIN_PORT=${CADDY_ADMIN_PORT:-8080}
